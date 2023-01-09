7da3f1194f7dd4653d337f46e8f03c6dcec5d6a1
noteMay <122172232+noteMay@users.noreply.github.com>
2023-01-09 15:45:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,8 @@ TODO_ISSUES_LABELS = ["TODO"]
 FRIENDS_LABELS = ["Links"]
 IGNORE_LABELS = FRIENDS_LABELS + TOP_ISSUES_LABELS + TODO_ISSUES_LABELS
 
-FRIENDS_TABLE_HEAD = "| Name | Link | \n | ---- | ---- |\n"
-FRIENDS_TABLE_TEMPLATE = "| {name} | {link} |\n"
-FRIENDS_INFO_DICT = {
-    "名字": "",
-    "链接": "",
-}
+FRIENDS_TABLE_HEAD = "Name：Link"
+FRIENDS_TABLE_TEMPLATE = "{name}：{link}\n"
 
 
 def get_me(user):
